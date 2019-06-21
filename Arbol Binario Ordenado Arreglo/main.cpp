7095152632163e051c1518d08896209de718b38e
daniii3012 <daniel_301999@hotmail.com>
2019-06-20 23:45:13 -0500

    Update main.cpp

--- Arbol Binario Ordenado Arreglo/main.cpp
+++ Arbol Binario Ordenado Arreglo/main.cpp
@@ -10,38 +10,15 @@ using namespace std;
 int main(int argc, char** argv) {
 	
 	arbolBin arbol(9);
-	arbol.imprimir();
 	
 	arbol.insertar(100);
-	cout << endl;
-	arbol.imprimir();
-	
 	arbol.insertar(90);
-	cout << endl;
-	arbol.imprimir();
-	
 	arbol.insertar(80);
-	cout << endl;
-	arbol.imprimir();
-	
 	arbol.insertar(110);
-	cout << endl;
-	arbol.imprimir();
-	
 	arbol.insertar(120);
-	cout << endl;
-	arbol.imprimir();
-	
 	arbol.insertar(95);
-	cout << endl;
-	arbol.imprimir();
-	
 	arbol.insertar(130);
-	cout << endl;
-	arbol.imprimir();
-	
 	arbol.insertar(105);
-	cout << endl;
 	arbol.imprimir();
 	
 	return 0;
