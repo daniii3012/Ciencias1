dfbebce01bfe3e57a90ce13c5f9c7d9218b3b943
daniii3012 <daniel_301999@hotmail.com>
2019-06-20 19:29:52 -0500

    Arbol Bin Ord

---
+++ Arbol Binario Ordenado Arreglo/arbol.h
@@ -0,0 +1,51 @@
+/*
+	Daniel Alejandro Montiel - 20171020049
+	Angie Gabriela Antolinez - 20151020061
+*/
+#ifndef ARBOL_H
+#define ARBOL_H
+#include <iostream>
+
+using namespace std;
+
+
+struct arbol_array{
+	int dato;
+	int izq;
+	int der;	
+};
+
+class arbol{
+	arbol_array *arbolBin;
+	int tam;
+	public:
+		arbol(int n){
+			tam = n;
+			int i;
+			arbolBin = new arbol_array[tam];
+			
+			for(i = 0; i < tam; i++){
+				arbolBin[i].dato = 0;
+				arbolBin[i].izq = 0;
+				arbolBin[i].der = 0;
+			}
+			
+			for(i = 0; i < tam-1; i++){
+				arbolBin[i].der = i+1;
+			}
+		}
+		
+		void imprimir();
+		
+};
+
+void arbol::imprimir(){
+	for(int i = 0; i < tam; i++){
+		cout << "Dato: " << arbolBin[i].dato << " ";
+		cout << "Izq: " << arbolBin[i].izq << " ";
+		cout << "Der: " << arbolBin[i].der << " ";
+		cout << endl;
+	}
+}
+
+#endif
---
+++ Arbol Binario Ordenado Arreglo/main.cpp
@@ -0,0 +1,17 @@
+/*
+	Daniel Alejandro Montiel - 20171020049
+	Angie Gabriela Antolinez - 20151020061
+*/
+#include <iostream>
+#include "arbol.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	arbol arbolBin(4);
+	
+	arbolBin.imprimir();
+	
+	return 0;
+}
