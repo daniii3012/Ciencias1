ffbfec5a6c5f1944af00559c70e9ba9dca7e5410
daniii3012 <daniel_301999@hotmail.com>
2019-06-28 09:57:04 -0500

    Colas de Prioridad

--- Colas de Prioridad/main.cpp
+++ Colas de Prioridad/main.cpp
@@ -5,8 +5,8 @@
 #include "cola_prioridad.h"
 using namespace std;
 
-int main()
-{
+int main(){
+	
 	PriorityQueue pq;
 	
 	while (1){
@@ -46,56 +46,6 @@ int main()
             cout<<"Ingrese una opcion correcta"<<endl;
         }
     }
-	/*
-	pq.push(60);
-	pq.push(80);
-	pq.push(20);
-	pq.push(90);
-	pq.push(50);
-	pq.push(40);
-	pq.push(30);
-	pq.push(70);
-	pq.push(55);
-	pq.push(45);
-	
-	pq.print();
-	cout << endl;
-	
-	cout << pq.top() << " " << endl;;
-	pq.pop();
-	
-	pq.print();
-	
-	
-	// Note - Priority is decided by element's value
-	
-	pq.push(3);
-	pq.push(2);
-	pq.push(15);
-	
-	cout << "Size is " << pq.size() << endl;	
-	
-	cout << pq.top() << " ";
-	pq.pop();
-	cout << pq.top() << " ";
-	pq.pop();
-	pq.push(5);
-	pq.push(4);
-	pq.push(45);
-	cout << endl << "Size is " << pq.size() << endl;
-	
-	cout << pq.top() << " ";
-	pq.pop();
-	cout << pq.top() << " ";
-	pq.pop();
-	cout << pq.top() << " ";
-	pq.pop();
-	cout << pq.top() << " ";
-	pq.pop();
-	cout << endl << std::boolalpha << pq.empty();
-	
-	pq.top();	// top operation on an empty heap
-	pq.pop();	// pop operation on an empty heap
-	*/
+    
 	return 0;
 }
